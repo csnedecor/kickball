088a9288dcc7c49347ad20586d5ef9c6dff0f912
Cori Snedecor <coriannas@yahoo.com>
2014-11-18 18:01:04 -0500

    Moved ruby logic to server.rb file

--- server.rb
+++ server.rb
@@ -1,19 +1,30 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require 'json'
 require 'pry'
-file = File.read("roster.json")
-roster = JSON.parse(file)
 
 get '/' do
+  file = File.read("roster.json")
+  @roster = JSON.parse(file)
+  @teams = []
+  @roster.each_key{|team| @teams << team}
+  @roster.each do |team, players|
+    @players = players
+  end
   erb :index
 end
 
 get '/teams/:team_name' do
+  file = File.read("roster.json")
+  @roster = JSON.parse(file)
   @team = params[:team_name]
+  @players = @roster[@team]
   erb :show
 end
 
 get '/position/:position_name' do
+  file = File.read("roster.json")
+  @roster = JSON.parse(file)
   @position = params[:position_name]
   erb :position
 end
